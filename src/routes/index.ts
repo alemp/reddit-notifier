f06278dad477e3a985fb4a8581291e15726749a9
Alessandro Pinto <alessandro.pinto@akelius.de>
2020-12-20 21:01:30 +0100

    removed unused endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Application, Request, Response } from 'express';
-import { RedditController } from '../controllers/reddit.controller';
 import channelRouter from './channel.router';
 import userRouter from './user.router';
 
@@ -14,24 +13,6 @@ export class Routes {
   register(app: Application): void {
     app.use('/channels', channelRouter);
     app.use('/users', userRouter);
-
-    app.use('/authenticate', async (req: Request, res: Response) => {
-      const controller = new RedditController();
-
-      const response = await controller.authenticate();
-
-      res.status(200).json(response);
-    });
-
-    app.use('/reddit/:channel', async (req: Request, res: Response) => {
-      const controller = new RedditController();
-
-      const channel = req.params.channel;
-
-      const response = await controller.getTopPostsFromChannel(channel);
-
-      res.status(200).json(response);
-    });
 
     app.use('/', (req: Request, res: Response) => {
       res.status(200).json({
